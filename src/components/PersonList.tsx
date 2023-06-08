9c0fb5df78c18036f3d70d46e61e1d31d72205b5
jafoor <abujafor.cse11@gmail.com>
2023-06-08 12:03:31 +0600

    props types and tips

--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -1,6 +1,9 @@
+import { Name } from "./Person.types";
+
 type PersonListProps = {
-  names: { first: string; last: string }[];
+  names: Name[];
 };
+
 export const PersonList = (props: PersonListProps) => {
   return (
     <div>
