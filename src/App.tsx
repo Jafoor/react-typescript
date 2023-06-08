793054d4994284cc1303ba0a321ea23ec0dfdbe4
jafoor <abujafor.cse11@gmail.com>
2023-06-08 11:51:27 +0600

    Style props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
+import { Container } from "./components/Container";
+
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => {
-          console.log("🚀 ~ file: App.tsx:12 ~ App ~ handleClick:", event, id);
-        }}
-      />
-      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
